1b1b436017782c8354cb280fa1ba5307c07e15c9
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-10 19:43:48 -0300

    Entities atributes corrected

--- Entities/Curso.cs
+++ Entities/Curso.cs
@@ -4,12 +4,12 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string descricao { get; set; }
+        public string Descricao { get; set; }
         public int Nivel { get; set; }
-        public double duracao { get; set; }
-        public int ProfessorID { get; set; }
+        public double Duracao { get; set; }
+        public int ProfessorId { get; set; }
         public DateTime DataInicio { get; set; }
-        public double valor { get; set; }
+        public double Valor { get; set; }
     }
 
 }
