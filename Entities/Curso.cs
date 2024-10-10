4f206c66efb983a88060f144a781f739c460e35c
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-10 20:15:59 -0300

    Curso atribute Nivel corrected to String

--- Entities/Curso.cs
+++ Entities/Curso.cs
@@ -5,11 +5,10 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public int Nivel { get; set; }
+        public string Nivel { get; set; }
         public int Duracao { get; set; }
         public int ProfessorId { get; set; }
         public DateTime DataInicio { get; set; }
         public double Valor { get; set; }
     }
-
 }
